91cf2758cc1ed96fac04abb579883004a60d25cd
Murat Bastas <muratbsts@gmail.com>
2017-05-10 01:19:26 +0300

    Ham kaynak

---
+++ config.rb
@@ -0,0 +1,41 @@
+require 'digest/md5'
+
+activate :directory_indexes
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+page '/topluluk'
+
+helpers do
+  def gravatar_for(email)
+    hash = Digest::MD5.hexdigest(email.chomp.downcase)
+    "http://www.gravatar.com/avatar/#{hash}?s=200"
+  end
+
+  def icon_for(icon)
+    "<i class=\"fa fa-#{icon}\"></i>"
+  end
+end
+
+activate :relative_assets
+set :relative_links, true
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = 'last 2 versions'
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.remote   = 'origin'
+  deploy.branch   = 'master'
+  # deploy.strategy = :submodule
+  deploy.build_before = true
+end
